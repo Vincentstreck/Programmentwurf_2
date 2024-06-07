aa33c1a7c3d353efc686c9e21288f1550d6f55eb
Vincentstreck <156666177+Vincentstreck@users.noreply.github.com>
2024-06-08 01:04:39 +0200

    rough prototype for the test: next step is to implement developer.cpp

--- test/test_developer.cpp
+++ test/test_developer.cpp
@@ -2,3 +2,19 @@
 #include "developer.hpp"
 #include "junior_developer.hpp"
 #include "senior_developer.hpp"
+
+
+
+TEST_F(DeveloperTest, TestGetName) {
+    EXPECT_EQ(junior.get_name(), "Dan Castellaneta");
+    EXPECT_EQ(senior.get_name(), "Nancy Cartwright");
+}
+
+TEST_F(DeveloperTest, TestGetAlias) {
+    EXPECT_EQ(junior.get_alias(), "Bart Simpson");
+    EXPECT_EQ(senior.get_alias(), "Homer Simpson");
+}
+
+TEST_F(DeveloperTest, TestLogoLoading) {
+    junior.load_logo_from_file("../logo/logo.txt");
+    EXPECT_EQ(junior.get_logo(), "Expected Logo Content");

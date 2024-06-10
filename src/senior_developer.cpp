ea9f797588793e242104050c41d3a383f25d78fa
Vincentstreck <156666177+Vincentstreck@users.noreply.github.com>
2024-06-10 09:00:28 +0200

    Implement senior developer class (similar to junior developer class)

--- src/senior_developer.cpp
+++ src/senior_developer.cpp
@@ -1,1 +1,24 @@
 #include "senior_developer.hpp"
+
+/**
+ * @class SeniorDeveloper
+ * @brief Senior developer with problem-solving capabilities.
+ */
+
+/**
+ * @brief Constructs a SeniorDeveloper.
+ * 
+ * @param name Developer's name.
+ * @param alias Developer's alias.
+ */
+SeniorDeveloper::SeniorDeveloper(const std::string& name, const std::string& alias) 
+    : Developer(name, alias) {}
+
+/**
+ * @brief Solves a problem and prints a message.
+ *  It calls the drink_coffee() function.
+ */
+void SeniorDeveloper::solve_problem() const {
+    std::cout << "Solving a problem:\n" << *this << "\nHa, that was EZ!" << std::endl;
+    drink_coffee();
+}

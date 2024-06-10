c36fa7151ed6151bf6a24f21a88f7353ede165b3
Vincentstreck <156666177+Vincentstreck@users.noreply.github.com>
2024-06-10 08:48:21 +0200

    Implement JuniorDeveloper class with problem-solving functionality

--- src/junior_developer.cpp
+++ src/junior_developer.cpp
@@ -1,1 +1,21 @@
 #include "junior_developer.hpp"
+
+/**
+ * @brief Constructor for JuniorDeveloper.
+ * 
+ * @param name The name of the junior developer.
+ * @param alias The alias of the junior developer.
+ */
+JuniorDeveloper::JuniorDeveloper(const std::string& name, const std::string& alias) 
+    : Developer(name, alias) {}
+
+/**
+ * @brief Solves a problem and outputs the process.
+ * 
+ * This function prints a message. 
+ * It calls the drink_coffee() function.
+ */
+void JuniorDeveloper::solve_problem() const {
+    std::cout << "Solving a problem:\n" << *this << "\nPuh, that was tough" << std::endl;
+    drink_coffee();
+}
